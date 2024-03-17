21ac17653ba3e07703f7d70fd14deab40e8c612b
“whisky39” <tiensktt1bang@gmail.com>
2024-03-17 15:03:34 +0700

    fix conflict

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -52,6 +52,7 @@ function Home() {
             </div>
           </div>
         </div>
+
         <div className="box">
           <div className="body">
             <div className="imgContainer">
@@ -60,23 +61,24 @@ function Home() {
                 alt=""
               />
             </div>
-            <div className="content d-flex flex-column align-items-center justify-content-center">
-              <div>
-                <h3 className="text-white fs-5">Post Title</h3>
-                <p className="fs-6 text-white">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Explicabo sed cum neque, rem provident ex. Laboriosam
-                  perspiciatis modi eveniet in?
-                </p>
+            <div className="content  d-flex flex-column align-items-center justify-content-center">
+              <div className="bg-red">
+                <h3 className="text-white fs-5">Phạm Gia Tiến</h3>
+                <p className="fs-6 text-white">CE170146</p>
+                <p className="fs-6 text-white">Front-end developer</p>
+                <Link to={"/tien"} className="text-color-tien">
+                  Detail
+                </Link>
               </div>
             </div>
           </div>
         </div>
+
         <div className="box">
           <div className="body">
             <div className="imgContainer">
               <img
-                src="https://images.pexels.com/photos/3601422/pexels-photo-3601422.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                src="https://images.pexels.com/photos/573238/pexels-photo-573238.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
                 alt=""
               />
             </div>
@@ -92,6 +94,7 @@ function Home() {
             </div>
           </div>
         </div>
+
         <div className="box">
           <div className="body">
             <div className="imgContainer">
@@ -112,6 +115,7 @@ function Home() {
             </div>
           </div>
         </div>
+
         <div className="box">
           <div className="body">
             <div className="imgContainer">
